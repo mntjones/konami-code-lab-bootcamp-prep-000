735c4332edd20a64fa470729974a9d458af03c20
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-21 22:06:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var check = []
+
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  var input = document.body(input)
+  var index = 0
+  
+  input.addEventListener('keydown', function(e) {
+    
+    var check = parseInt(e.detail || e.which)
+    for (let i=0; i< code.length; i++) {
+      if (check == array[index]){
+        index ++ 
+        if (index == code.length) {
+          alert("Konami Code Master!")
+          
+          index = 0
+        }
+      }
+      
+      else { index = 0 }
+    }
+  })
+  
+   
 }
